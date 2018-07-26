f9500c27704d6e1a8adf487a1639718e74cd2d27
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-07-26 16:37:56 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=8;
+		System.out.println("Enter a value");
+		if(a>0)
+		System.out.println("Given value is +ve");
+		if (a<0)
+		System.out.println("given value is -ve");
+		if(a==0)
+		System.out.println("given value is 0");
+	}
+}
