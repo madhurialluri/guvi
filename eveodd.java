8def61bb66accd62de43e2250e885bbc08f7b515
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-07-27 09:46:06 +0530

    Create eveodd.java

---
+++ eveodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		Scanner Sc=new Scanner(System.in);
+		int n;
+		n=Sc.nextInt();
+		System.out.println("enter a number");
+		if(n%2==0)
+		System.out.println("given num is even");
+		else
+		System.out.println("given num is odd");
+		
+	}
+}
