8bcbf91d9cb1ff18f3869d4253ea7f79c9079136
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-14 12:19:31 +0530

    Create test37.java

---
+++ test37.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a, b, temp;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		b = sc.nextInt();	
+		{
+		temp = a;
+		a = b;
+		b = temp;
+		}
+		System.out.println(a+" "+b);
+	}
+}
