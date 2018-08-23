a7b06faaf941893033cc03498d8ca5f0b4b584dc
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-23 10:10:49 +0530

    Create test62.java

---
+++ test62.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int r=0,c=0,num,b; 
+              Scanner sl=new Scanner(System.in); 
+              num=sl.nextInt(); 
+              b= num; 
+              while(num>0) 
+                   { 
+                      if((num %10==0)|| (num%10==1)) 
+                         c++; 
+                         r++; 
+                         num=num/10; 
+                   } 
+                      if(c==r) 
+               System.out.println("yes"); 
+               else 
+               System.out.println("no");
+
+	}
+}
