d9a8e7d8fae217083d987104ec3ac6d1c136c9de
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-25 10:36:54 +0530

    Create test73.java

---
+++ test73.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class print73
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int m=sc.nextInt();
+		int n=sc.nextInt();
+		if(a>m&&a<n)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
