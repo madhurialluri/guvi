2cf069f45aaa86a36168a5d372720b71f76862e9
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-25 11:10:45 +0530

    Create test82.java

---
+++ test82.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class test82
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		double l=sc.nextDouble();
+		double b=sc.nextDouble();
+		double area=l*b;
+		System.out.format("%.5f",area);
+	}
+}
