a72b61285e258b4e9758699d75c2b84c5c98f48c
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-29 12:14:48 +0530

    Create test70.java

---
+++ test70.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class test70
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int pow=2*n;
+		System.out.println(pow);
+	}
+}
