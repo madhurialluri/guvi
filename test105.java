e03ee05cebb435bfd50d13d770797f3af84d121d
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-08-29 12:22:53 +0530

    Create test105.java

---
+++ test105.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class test105
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b;
+	            Scanner sc=new Scanner(System.in);
+	            a=sc.nextInt();
+	            b=sc.nextInt();
+	            System.out.println(a+""+b);
+	}
+}
