b55f56c63f5a55c4bde84cf7879511731a21da7a
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-09-06 16:23:46 +0530

    Create test86.java

---
+++ test86.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class test86
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String st=sc.nextLine();
+		int count=0;
+		for(int i=0;i<st.length()-1;i++)
+		{
+			if(st.charAt(i)==st.charAt(i+1))
+			{
+				count++;
+			}
+		}
+			if(count==0)
+			System.out.println("Yes");
+			else
+			System.out.println("No");
+	}
+}
