feafcbe34b7f05de6adb0d8d0e35a05d58fff923
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-09-07 14:33:27 +0530

    Create test104.java

---
+++ test104.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class teest104
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		    Scanner sc=new Scanner(System.in);
+		    int k=sc.nextInt();
+		    int n=sc.nextInt();
+		    int pow=1;
+		    while(n !=0)
+		    {
+			      pow *=k;
+			      --n;
+		    }
+		    System.out.println(pow);
+
+	}
+}
