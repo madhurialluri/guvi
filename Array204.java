08af2e18de80447da1227b96fe172d7a943dd81b
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-09-12 10:48:38 +0530

    Create Array204.java

---
+++ Array204.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Array204
+{
+	    public static void main (String[] args) throws java.lang.Exception
+	   {
+		    Scanner sc=new Scanner(System.in);
+		    int n=sc.nextInt();
+		    int arr[]=new int[n];
+		    int sum=0;
+		    for(int i=0;i<n;i++)
+		    {
+			      arr[i]=sc.nextInt();
+				    if(arr[i]<0)
+					  sum +=arr[i];
+		    }
+			  System.out.println(sum);
+
+	   }
+}
