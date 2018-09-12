bf2cd9594fef45ad36037db686e00d151203639d
madhurialluri <41733163+madhurialluri@users.noreply.github.com>
2018-09-12 10:43:12 +0530

    Create catalan201.java

---
+++ catalan201.java
@@ -0,0 +1,38 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class catalan201
+{
+	   public static void main (String[] args) throws java.lang.Exception
+	   {
+	      Scanner sc=new Scanner(System.in);
+		    int n=sc.nextInt();
+		    long n3,n4,a;
+		    long CatNumber;
+		    int cnt=1;
+		    if(n>=0&&n<=10)
+		    {
+			     for(a=0;a<=n;a++)
+			     {
+				      long factofa=1,factof2a=1;
+				      for(long i=1;i<=a;i++)
+					    factofa*=i;
+				      for(long i=1;i<=2*a;i++)
+					    factof2a*=i;
+				      CatNumber=factof2a/((factofa*factofa)*(a+1));
+				      if(a==n)
+				      {
+					       System.out.print(CatNumber);
+				      }
+				      else
+				      {
+					       System.out.print(CatNumber+" ");
+				      }
+			    }
+		    }
+	   }
+}
